c48e0d22568252c9b36abf328977185fe15f1483
Peter-Dodge <ptr.dodge@gmail.com>
2022-06-10 14:10:21 -0700

    keymap function

--- keyboard.js
+++ keyboard.js
@@ -3,53 +3,8 @@ class Keyboard {
         this.element = element ? element : document
         this.key = ""
     }
-    // deciding on wether or not to use this
-/*
-    on(key, type, callback) {
-        type = type ? type : "keydown" || "keyup"
-        this.key = key
-        key = key ? key : "" || []
-        callback = callback ? callback : () => console.log(key)
-        this.element.addEventListener(type, function (e) {
-            for (let n = 0; n < key.length; n++) {
-                if (e.key == key[n]) {
-                    if (typeof callback == 'function') {
-                        return callback()
-                    } else return console.error("please provide a function as a callback")
-                }
-            }
-        })
-    }
 
-    send(keyWanted, type) {
-        type = type ? type : "keydown" || "keyup"
-        var mds = { ctrlKey: false, shiftKey: false, altKey: false },
-            callback = () => console.log(keyWanted),
-            modifiers = []
-
-        if (typeof arguments[2] == 'array') { modifiers = arguments[2] }
-        if (typeof arguments[2] == 'function') { callback = arguments[2] }
-        if (typeof arguments[3] == 'array') { modifiers = arguments[3] }
-        if (typeof arguments[3] == 'function') { callback = arguments[3] }
-
-        for (let n = 0; n < modifiers.length; n++) {
-            modifiers[n] === 'ctrl' ? mds.ctrlKey = true : null
-            modifiers[n] === 'shift' ? mds.shiftKey = true : null
-            modifiers[n] === 'alt' ? mds.altKey = true : null
-        }
-
-        var keyEvent = new KeyboardEvent(type, { key: keyWanted, repeat: false, target: this.element, char: keyWanted, ctrlKey: mds.ctrlKey, shiftKey: mds.shiftKey, altKey: mds.altKey })
-        this.element.dispatchEvent(keyEvent)
-        this.element.innerText = keyWanted
-        console.log(keyEvent)
-
-        if (typeof callback == 'function') {
-            return callback()
-        } else return console.error("please provide a function as a callback")
-    }
-*/
-
-    keydown(key, callback,) {
+    keydown(key, callback) {
         this.key = key
         key = key ? key : "" || []
         callback = callback ? callback : () => console.log(key)
@@ -129,5 +84,19 @@ class Keyboard {
         if (typeof callback == 'function') {
             return callback()
         } else return console.error("please provide a function as a callback")
+    }
+
+    keymap(keyArr, type, callbackArr) {
+        type = type ? type : "keydown" || "keyup"
+        keyArr = keyArr ? keyArr : []
+        callbackArr = callbackArr ? callbackArr : [() => console.log(keyArr)]
+        for (let n = 0; n < keyArr.length; n++) {
+            if (type == "keydown") {
+                this.keydown(keyArr[n], callbackArr[n])
+            }
+            if (type == "keyup") {
+                this.keyup(keyArr[n], callbackArr[n])
+            }
+        }
     }
 }
